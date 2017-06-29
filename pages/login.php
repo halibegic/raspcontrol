23345af62ae9b67ed868e677f37c0a9ff8ee9664
Hasan Alibegić <hasan.alibegic@edu.fit.ba>
2017-06-29 21:55:29 +0200

    New login

--- pages/login.php
+++ pages/login.php
@@ -1,9 +1,31 @@
-<div class="container">
-    <div class="center">
-        <form class="form-inline form-login" method="post" action="<?php echo LOGIN; ?>">
-            <input type="text" name="username" id="username" placeholder="Username" autofocus />
-            <input type="password" name="password" id="password" placeholder="Password" />
-            <input type="submit" class="btn btn-inverse" value="Connect" />
+<div class="row">
+
+    <div class="col-sm-6 offset-sm-3">
+
+        <form method="post" action="<?php echo LOGIN; ?>">
+
+            <div class="form-group row">
+                <label for="username" class="col-sm-2 col-form-label">Username</label>
+                <div class="col-sm-10">
+                    <input class="form-control" id="username" type="text" name="username" autofocus>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="password" class="col-sm-2 col-form-label">Password</label>
+                <div class="col-sm-10">
+                    <input type="password" class="form-control" id="password">
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <div class="offset-sm-2 col-sm-10">
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                </div>
+            </div>
+
         </form>
+
     </div>
+
 </div>
